500350534350d2fbf2cc8135963a4ba3b859e66c
nachosteeman@gmail.com <nachosteeman@gmail.com>
2025-11-20 13:06:07 -0300

    Arregle unas cosas de b2b

--- src/Simplytyped.hs
+++ src/Simplytyped.hs
@@ -42,14 +42,24 @@ conversion (LNil)       = Nil
 conversion (LCons n m)  = Cons (conversion n) (conversion m)
 
 
+
+
+
+
+
+
+
+
+
 -- b2b: Convierte de Brujin a Bound ?????????? VER
 b2b :: Name -> Int -> Term -> Term
 b2b idx i (Bound n)       = (Bound n)
-b2b idx i (Free name)     = if (idx == name) then (Bound i)
-                                             else (Free name)
+b2b idx i (Free name)     = if (idx == name) then (Bound i) else (Free name)
 b2b idx i (t1 :@: t2)     = (b2b idx i t1) :@: (b2b idx i t2)
 b2b idx i (Lam t term) = Lam t (b2b idx (i+1) term)
+
 b2b idx i (Let t1 t2)     = Let (b2b idx i t1) (b2b idx (i+1) t2)
+
 b2b idx i Zero            = Zero
 b2b idx i (Suc n)         = Suc (b2b idx i n)
 b2b idx i (Rec n m p)     = Rec (b2b idx i n) (b2b idx i m) (b2b idx i p)
@@ -79,27 +89,37 @@ quote (VNum (NSuc n)) = Suc (quote (VNum n))
 quote VNil = Nil
 quote (VCons n xs) = Cons (quote (VNum n)) (quote (VList xs))
 
+
+
+
+
+
+
+
+
+
+
 -- evalúa un término en un entorno dado
 eval :: NameEnv Value Type -> Term -> Value
 eval e (Bound i) = error ("Variable ligada fuera de alcance: " ++ show i)
 eval e (Free name )    = case lookup name e of
-    Just (v, _) -> v
-    Nothing     -> error ("Variable no definida: " ++ show name)
-
+                            Just (v, _) -> v
+                            Nothing     -> error ("Variable no definida: " ++ show name)
 eval e (Lam t term)    = VLam t term                                        
---eval (NameEnv nvs t) (u :@: v)       = case (eval u) of 
---                (VLam t term) -> eval e (sub 0 v term )
-
--- Claudio: 
 eval e (t :@: u)     = let  (VLam _ body) = eval e t 
                        in   eval e (sub 0 (quote (eval e u)) body)
 
+-- Seccion 8:
+eval e (Let t1 t2) = let v = eval e t1
+                     in (sub 0 (quote v) t2) 
+
+-- Seccion Nat
 eval e Zero      = VNum NZero
 eval e (Suc t)   = case eval e t of
                      VNum n -> VNum (NSuc n)
                      _      -> error "Suc aplicado a no-numero"
 
-
+-- Seccion List:
 eval e Nil         = VList VNil
 eval e (Cons t u)  = VList (VCons (eval e t) (eval e u))
 
@@ -125,8 +145,7 @@ ret = Right
 err :: String -> Either String Type
 err = Left
 
-(>>=)
-  :: Either String Type -> (Type -> Either String Type) -> Either String Type
+(>>=) :: Either String Type -> (Type -> Either String Type) -> Either String Type
 (>>=) v f = either Left f v
 -- fcs. de error
 
@@ -147,14 +166,79 @@ notfoundError n = err $ show n ++ " no está definida."
 
 -- infiere el tipo de un término a partir de un entorno local de variables y un entorno global
 infer' :: Context -> NameEnv Value Type -> Term -> Either String Type
+
+-- Reglas de tipado Basicas: 
 infer' c _ (Bound i) = ret (c !! i)
 infer' _ e (Free  n) = case lookup n e of
   Nothing     -> notfoundError n
   Just (_, t) -> ret t
+
 infer' c e (t :@: u) = infer' c e t >>= \tt -> infer' c e u >>= \tu ->
   case tt of
     FunT t1 t2 -> if (tu == t1) then ret t2 else matchError t1 tu
     _          -> notfunError tt
+
 infer' c e (Lam t u) = infer' (t : c) e u >>= \tu -> ret $ FunT t tu
 
+-- Reglas de tipado para Let:
+infer' c e (Let term1 term2) =  infer' c e term1 >>= \t1 -> 
+                                infer' (t1:c) e term2 
+
+-- Reglas de tipado para Nat:
+infer' _ _ Zero    = ret NatT
+
+infer' c e (Suc t) = infer' c e t >>= \tType ->
+            case tType of 
+              NatT -> ret NatT
+              errType    -> notfunError errType
+
+
+infer' c e (Rec t1 t2 t3) = do t1Type <- infer' c e t1
+                               t2Type <- infer' c e t2
+                               t3Type <- infer' c e t3
+                            case t3Type of
+                              NatT ->  case t2Type of
+                                          FunT t1Type' (FunT NatT t1Type'') -> if t1Type' == t1Type 
+                                                                                    then  if t1Type == t1Type''
+                                                                                             then ret t1Type
+                                                                                             else matchError t1Type t1Type''
+                                                                                  else matchError t1Type t1Type'
+                                          _ -> notfunError t2Type
+                              
+                            
+                              _    -> matchError NatT t3Type 
+                               
+
+-- Reglas de tipado para Listas:
+infer' c e Nil          = ret ListT
+infer' c e (Cons t1 t2) = do t1Type <- infer' c e t1 
+                             t2Type <- infer' c e t2 
+ 
+                             case t1Type of
+                                NatT -> case t2Type of
+                                          ListT -> ret ListT
+                                          _     -> matchError ListT t2Type
+                                
+                                _    -> matchError NatT t1Type
+
+
+infer' c e (RecL t1 t2 t3) = do t1Type <- infer' c e t1 
+                                t2Type <- infer' c e t2 
+                                t3Type <- infer' c e t3
+
+                                case t3Type of
+                                   ListT ->  case t2Type of
+                                               FunT NatT (FunT ListT (FunT t1Type' t1Type'')) -> if t1Type' == t1Type 
+                                                                                                     then  if t1Type == t1Type''
+                                                                                                              then ret t1Type
+                                                                                                              else matchError t1Type t1Type''
+                                                                                                     else matchError t1Type t1Type'
+                                               _ -> notfunError t2Type
+
+
+                                   _    -> matchError ListT t3Type             
+
+-- Modularizar funcion chequeo Recursivo pasandole el tipo que espera para t3.                       
+
+                      
 
